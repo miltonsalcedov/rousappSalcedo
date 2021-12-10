e05a1c55655627b78fe20149f082d5214e81d501
unknown <miltonsalcedov@gmail.com>
2021-12-10 02:06:28 -0500

    Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar style={{backgroundColor: '#fcd5ce'}} expand="lg">
+  <Container fluid>
+    <img src='../assets/logorous.png'/>
+    <Navbar.Brand href="#">Rous beauty</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Accesorios</Nav.Link>
+        <NavDropdown title="Rostro" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Limpiadores Faciales</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Bases</NavDropdown.Item>
+         
+        </NavDropdown>
+       
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Bucar"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button style={{backgroundColor: '#e29578'}} variant="">Buscar</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+  );
+};
+
+export default NavBar;
